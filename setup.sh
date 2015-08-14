41b358d918acb14c874c30c241cc4a9ab9c2ec5c
Florian ALONSO <alonso.floo@gmail.com>
2015-08-14 17:11:27 +0200

    Added auto update

--- setup.sh
+++ setup.sh
@@ -15,14 +15,16 @@ done;
 
 unset file
 
-user_value=""
-echo "Do you want to install ZSH Shell ? (yes/no) "
-read user_value
-if [[ "$user_value" == "yes" ]]; then
-	if [[-x /usr/bin/apt-get ]]; then
-		sudo apt-get install -y zsh
-	else
-		ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-		brew install zsh zsh-completions
+if [[ "$#" -ne 1 ]] && [[ "$1" != "--quiet" ]]; then
+	user_value=""
+	echo "Do you want to install ZSH Shell ? (yes/no) "
+	read user_value
+	if [[ "$user_value" == "yes" ]]; then
+		if [[-x /usr/bin/apt-get ]]; then
+			sudo apt-get install -y zsh
+		else
+			ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+			brew install zsh zsh-completions
+		fi
 	fi
 fi
