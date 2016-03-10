d950f51f2b506846350274769f89daf564fcf20e
Florian ALONSO <alonso.floo@gmail.com>
2016-03-10 15:47:37 +0100

    Added ssh configuration

--- setup/common.sh
+++ setup/common.sh
@@ -1,5 +1,5 @@
 #setting links
-for file in ${DOTFILE_PATH}{bash_logout,bashrc,bash_profile,zshrc,zlogout,inputrc,gitconfig,vimrc,vim,config}; do
+for file in ${DOTFILE_PATH}{bash_logout,bashrc,bash_profile,zshrc,zlogout,inputrc,gitconfig,vimrc,vim,config,ssh}; do
 	file="$( basename $file )"
 	
 	if [[ ! -h ~/.${file} ]] && [[ -d ~/.${file} ]]; then
