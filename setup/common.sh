6868a6e3f94cb238cbe4f4eed0e1e1dd533048e2
Florian ALONSO <alonso.floo@gmail.com>
2025-11-21 20:23:02 +0100

    fix: update gemini fake setup

--- setup/common.sh
+++ setup/common.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #setting links
-for file in ${DOTFILE_PATH}{bash_logout,bashrc,bash_profile,zshrc,zlogout,inputrc,gitconfig,vimrc,vim,config,ssh,gemini}; do
+for file in ${DOTFILE_PATH}{bash_logout,bashrc,bash_profile,zshrc,zlogout,inputrc,gitconfig,vimrc,vim,config,ssh}; do
 	file="$( basename $file )"
 
 	if [[ ! -h ~/.${file} ]] && [[ -d ~/.${file} ]]; then
