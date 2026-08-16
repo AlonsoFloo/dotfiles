1b2961e7d2b0d54b623f4229ed4b27f13e5d583a
Florian ALONSO <alonso.floo@gmail.com>
2026-08-16 11:32:16 +0200

    fix: Improved apm installation to be global

--- setup/common_postinstall.sh
+++ setup/common_postinstall.sh
@@ -4,5 +4,5 @@
 if command -v apm &>/dev/null; then
 	echo "Managing APM skills in ..."
 
-    apm install --update --force || true
+    apm install -g https://github.com/AlonsoFloo/skills#main --update --force --target agent-skills || true
 fi
