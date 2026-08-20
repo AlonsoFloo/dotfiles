158a530ea4c21fe33e65bf29d35513b1bf06d8d0
Florian ALONSO <alonso.floo@gmail.com>
2026-08-20 19:38:10 +0200

    chore: improved agent structure

--- setup/common_postinstall.sh
+++ setup/common_postinstall.sh
@@ -2,7 +2,7 @@
 
 # Manage local agent skills
 if command -v apm &>/dev/null; then
-	echo "Managing APM skills in ..."
+	echo "Managing APM skills ..."
 
     apm install -g https://github.com/AlonsoFloo/skills#main --update --force --target agent-skills || true
 fi
